81b05f4097d6d92639abb26ecc2472a44df505be
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-04-15 00:02:08 +0545

    modifing marks as complete button for transition of the lesson

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,6 @@
 class LessonsController < ApplicationController
   before_action :set_lesson, only: %i[show update]
+  before_action :set_course
 
   # GET /lessons or /lessons.json
 
@@ -7,6 +8,7 @@ class LessonsController < ApplicationController
   # GET /lessons/1 or /lessons/1.json
   def show
     @course = @lesson.course
+
   end
 
   # GET /lessons/new
@@ -19,12 +21,23 @@ class LessonsController < ApplicationController
 
   # PATCH/PUT /lessons/1 or /lessons/1.json
   def update
-
+    @lesson_user = LessonUser.find_or_create_by(user: current_user, lesson: @lesson)
+    @lesson_user.update!(completed: true)
+    next_lesson = @course.lessons.where("position > ?", @lesson.position).order(:position).first
+    if next_lesson
+      redirect_to course_lesson_path(@course, next_lesson)
+    else
+      redirect_to course_path(@course), notice: "You've completed the course"
+    end
   end
 
   # DELETE /lessons/1 or /lessons/1.json
 
   private
+
+  def set_course
+    @course = Course.find(params[:course_id])
+  end
     # Use callbacks to share common setup or constraints between actions.
     def set_lesson
       @lesson = Lesson.find(params[:id])
