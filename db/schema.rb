9c54b901c85e08d46921ef19fb332403cbd0e2f3
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-04-14 23:45:24 +0545

    adding model name lessonuser

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_14_160611) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_14_175823) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -77,6 +77,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_14_160611) do
     t.text "premium_description"
   end
 
+  create_table "lesson_users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "lesson_id", null: false
+    t.bigint "user_id", null: false
+    t.boolean "completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lesson_id"], name: "index_lesson_users_on_lesson_id"
+    t.index ["user_id"], name: "index_lesson_users_on_user_id"
+  end
+
   create_table "lessons", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -103,5 +113,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_14_160611) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "lesson_users", "lessons"
+  add_foreign_key "lesson_users", "users"
   add_foreign_key "lessons", "courses"
 end
